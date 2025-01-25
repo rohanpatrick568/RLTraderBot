ed18f789419d0682e0098437c6321f1fcea0b289
RJ <108032895+ZentaiMasta@users.noreply.github.com>
2025-01-25 13:07:30 -0500

    adding important assets

--- rl_strategy.py
+++ rl_strategy.py
@@ -1,54 +1,53 @@
 import numpy as np
-import os
+import pandas as pd
+
+from config import ALPACA_CONFIG
 from datetime import datetime, timedelta
+
+from lumibot.brokers import Alpaca
+from lumibot.traders import Trader
 from lumibot.strategies import Strategy
 from lumibot.backtesting import YahooDataBacktesting
 
 from gymnasium.spaces import Box
+
 from stable_baselines3 import PPO
 
 class PPO_Strategy(Strategy):
     def initialize(self, short_window, long_window, signal_window):
-        # Set initial parameters
         self.sleeptime = '1h'
         self.symbol = self.parameters['symbol']
         self.quantity = 10
         self.data_length = 50
-
-    def initialize(self):
-        # Load the pre-trained PPO model
-        model_path = self.parameters.get('ppo_model.zip')
-        if model_path is None or not os.path.exists(model_path):
-            raise ValueError(f"Model path '{model_path}' is invalid or does not exist.")
-        self.model = PPO.load(model_path)
+        self.timestep = self.parameters.get('timestep', 'hour')
+        self.model = PPO.load(self.parameters['ppo_model.zip'])
      
     def on_trading_iteration(self):
-        # Fetch historical price data
+        # Fetch data
         bars = self.get_historical_prices(
             self.symbol, 
-            length=self.data_length,  # Get 50 previous bars
+            length= self.data_length,  # Get 50 previous bars
             timestep=self.timestep
         )
         
-        # Define observation space for the model
+        # Define observation space
         low = np.full((self.data_length,), -np.inf)  # Minimum values for each element in the observation
         high = np.full((self.data_length,), np.inf)  # Maximum values for each element in the observation
         observation_space = Box(low=low, high=high, dtype=np.float32)
         
-        # Use the model to predict the next action
+        # Use model to trade
         action, _states = self.model.predict(bars, deterministic=True)
         
-        # Execute the action
+        # Execute trade
         if action == 1:
             self.buy(self.symbol, self.quantity)
         elif action == 0:
             self.sell(self.symbol, self.quantity)
         else:
             pass
-
-# Define the backtesting period
+        
+        
 end = datetime.now()
 start = end - timedelta(days=50)
 
-# Run the backtest
 PPO_Strategy.backtest(YahooDataBacktesting, start, end, parameters={'symbol': 'AAPL', 'timestep': 'hour'})
